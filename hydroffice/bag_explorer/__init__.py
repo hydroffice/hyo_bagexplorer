71822e35893a4d55a5322438e9240ac7d18c0314
gmasetti <gmasetti@ccom.unh.edu>
2015-12-16 19:50:15 -1000

    Minor fixes

--- hydroffice/bag_explorer/__init__.py
+++ hydroffice/bag_explorer/__init__.py
@@ -4,6 +4,7 @@ BAG Explorer
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os
 import logging
 
 log = logging.getLogger(__name__)
@@ -16,7 +17,13 @@ __license__ = 'BSD license'
 __copyright__ = 'Copyright 2015 University of New Hampshire'
 
 
-# def hyo():
 def hyo_app():
-# def hyo_lib():
     return __doc__, __version__
+
+
+# def hyo_lib():
+#     return __doc__, __version__
+
+
+def hyo_favicon():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "media", "hyo_button.png"))
